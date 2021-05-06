7f5c8a2e00436db63e3994581ed1bc857ba88163
Matthew Webb <mwebb2321@gmail.com>
2021-05-05 19:32:35 -0700

    some mods

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
+const APIKEY = "?results=";
 
 // Export an object with a "search" method that searches the Giphy APU for the passed query
 export default {
   search: function () {
-    return axios.get(BASEURL);
+    return axios.get(BASEURL + APIKEY + query);
   },
 };
